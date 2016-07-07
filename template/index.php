7d870f0f22d63394bf5656d35f2c6458a0efba56
Liza Zmievskaya <zmievskaya.liza@mail.ru>
2016-07-07 16:45:07 +0300

    Exceptions.

--- template/index.php
+++ template/index.php
@@ -1,8 +1,28 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once "template.php";
-$header = template('header.phtml', ['title' => 'Hello World!']);
-$content = template('content.phtml', ['content' => "Lorem ipsum", 'meta' => 'Author info']);
-$footer = template('footer.phtml', ['copy' => "Copyright ". date('Y')]);
-echo $header, $content, $footer;
+set_error_handler("errorHandler", E_ALL);
+function errorHandler($errno, $errstr, $errfile='', $errline=null, $errcontext=[]) //необязательные аргументы инициализируем
+{
+    /*require_once "template.php";
+    $header = template('header.phtml', ['title' => 'Hello World!']);
+    $content = template('content.phtml', ['content' => "Lorem ipsum", 'meta' => 'Author info']);
+    $footer = template('footer.phtml', ['copy' => "Copyright ". date('Y')]);
+    echo $header, $content, $footer;
+    nonexistingFunction();*/
+    if (error_reporting())
+    {
+        throw new Exception('Error', 111);
+    }
+}
+try
+{
+    echo $foo;
+    //nonexistingFunction();
+} catch (Exception $e) {
+    echo "<pre>";
+    var_dump($e);
+    echo $e->getMessage(), "\n";
+} finally {
+    echo "\r\n Hello!";
+}
